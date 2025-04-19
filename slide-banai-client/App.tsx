d1a0ef577a90d6ddf53fe552109233d82421e4cd
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-04-20 02:58:35 +0600

    modify app component

--- slide-banai-client/App.tsx
+++ slide-banai-client/App.tsx
@@ -1,9 +1,13 @@
 import { Switch, Route } from "wouter";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import AuthPage from "@/pages/auth-page";
 import ApiTestPage from "@/pages/api-test-page";
 import CanvaPage from "@/pages/canva-page";
 import { ProtectedRoute } from "./lib/protected-route";
+import { AuthProvider } from "./hooks/use-auth";
 
 
 function Router() {
@@ -16,3 +20,17 @@ function Router() {
     </Switch>
   );
 }
+
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
