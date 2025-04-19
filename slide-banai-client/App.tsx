01a36a54ec36a7b756e9b7984ea603f9e80f1c57
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2025-04-20 02:54:59 +0600

    added canva page and not found page, modify app component

--- slide-banai-client/App.tsx
+++ slide-banai-client/App.tsx
@@ -1,22 +1,18 @@
-import { Toaster } from "@/components/ui/toaster";
+import { Switch, Route } from "wouter";
+import NotFound from "@/pages/not-found";
+import AuthPage from "@/pages/auth-page";
+import ApiTestPage from "@/pages/api-test-page";
+import CanvaPage from "@/pages/canva-page";
+import { ProtectedRoute } from "./lib/protected-route";
+
 
 function Router() {
   return (
     <Switch>
-
+      <Route path="/auth" component={AuthPage} />
+      <ProtectedRoute path="/canva" component={CanvaPage} />
+      <ProtectedRoute path="/api-test" component={ApiTestPage} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Router />
-        <Toaster />
-      </AuthProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
